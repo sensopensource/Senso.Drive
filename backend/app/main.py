d744288e01b357ad0496f3c146e3d66f41a7134d
Ayoub <souidi.ayoub.22@gmail.com>
2026-04-15 00:06:20 +0100

    feat: ajout docker-compose, Dockerfile backend et endpoint /health

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from stage!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/health")
+def health():
+    return {"Status", "ok"}
---
+++ docker/postgres/init.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS unaccent;
+-- pour gerer les accents
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+-- pour gerer les fautres d'orthographes
+
+CREATE TEXT SEARCH CONFIGURATION french_unaccent (COPY = french);
+-- jai copie la config classque de french,pourchaque hword(mot compose),hword-part(partie de mot compose),mot classique japplique         
+-- le traitement unaccent pour elenver les accents,ensuite stem pour tokeniser ou plutot garder le mot a sa racine
+ALTER TEXT SEARCH CONFIGURATION french_unaccent
+  ALTER MAPPING FOR hword,hword_part,word
+  WITH unaccent, french_stem;
+ 
